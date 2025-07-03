7bab8c5812d5aed82afe61b0535c33205a4f2d2c
OmarJeghale <omarjeghalef05@gmail.com>
2025-07-02 20:07:04 -0400

    Added computer choice functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!")
+let random = Math.random()
+function getComputerChoice() {
+
+    if (random >= 0 && random <= 0.33) {
+        return 'rock'
+    }
+
+    if (random > 0.33 && random <= 0.66) {
+        return 'paper'
+    }
+
+    if (random > 0.66 && random <= 1) {
+        return 'scissors'
+    }
+}
